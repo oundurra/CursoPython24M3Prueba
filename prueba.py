a980bb2b0ba506944e0d320677394f24b2d74a93
Oscar Undurraga <oscarua@gmail.com>
2024-04-23 19:32:01 -0400

    Initial Commit

--- prueba.py
+++ prueba.py
@@ -9,7 +9,7 @@ def get_page_html():
     html = '''<!DOCTYPE html>
             <html>
             <head>
-                <title>Título de la Página</title>
+                <title>Aves de Chile</title>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">            </head>
             <body>
                 <nav class="navbar bg-body-tertiary">
